7971cc9a6916a4813f84a7bbe92fdd3850acba0d
Rodrigo Medina <amerpad12@MacBook-Pro-de-Rodrigo.local>
2018-01-10 13:32:50 -0600

    Adelanto del Menu de navegacion

--- src/app/menu-navegacion/menu-navegacion.component.ts
+++ src/app/menu-navegacion/menu-navegacion.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewChecked } from '@angular/core';
+declare var $: any;
 
 @Component({
-  selector: 'app-menu-navegacion',
+  selector: 'menu-navegacion',
   templateUrl: './menu-navegacion.component.html',
   styleUrls: ['./menu-navegacion.component.css']
 })
+
+
+
 export class MenuNavegacionComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+    $(".button-collapse").sideNav();
+    $("#buscadorMenu").hide();
   }
+
+  apareceBuscador():void{
+    $("#buscadorMenu").slideToggle();
+  }
+
 
 }
