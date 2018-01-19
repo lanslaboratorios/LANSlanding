35fb678251a1bc3c0824f5943f8a9a4098dc7581
Cristian Espiritu <cxistian@gmail.com>
2018-01-18 18:13:04 -0600

    primer esqueleto socios

--- src/app/socios/socios.component.ts
+++ src/app/socios/socios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'socios',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SociosComponent implements OnInit {
 
+  beneficios:any[];
+  socios:any[];
+
   constructor() { }
 
   ngOnInit() {
+    //$('#carr').carousel({fullWidth: true});
+    this.beneficios=[{'imagen':'../assets/socios/moduloweb.png', 'texto':'MÓDULO WEB PARA CAPTURA DE ÓRDENES'},
+    {'imagen':'../assets/socios/apoyo.png', 'texto':'APOYO EN CAMPAÑAS (PRECIOS ESPECIALES)'},
+    {'imagen':'../assets/socios/credito.png', 'texto':'CRÉDITO'},
+    {'imagen':'../assets/socios/descuento.png', 'texto':'DESCUENTOS EN ESTUDIOS DE LABORATORIO PARA SU PERSONAL'},
+    {'imagen':'../assets/socios/integracion.png', 'texto':'INTEGRACIÓN DE SISTEMA DE LABORATORIO'},
+    {'imagen':'../assets/socios/reporte.png', 'texto':'REPORTE DE RESULTADOS PERSONALIZADO'}];
+  }
+
+  anterior():void{
+    $('#scar').carousel('next',1);
+  }
+  siguiente():void{
+    $('#scar').carousel('prev',1);
   }
 
 }
