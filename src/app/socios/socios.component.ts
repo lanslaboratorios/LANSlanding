b24315e8ea10f07b2cb693b03010b8eeaea3aed4
Cristian Espiritu <cxistian@gmail.com>
2018-01-23 15:33:06 -0600

    animacion perks realizada

--- src/app/socios/socios.component.ts
+++ src/app/socios/socios.component.ts
@@ -10,17 +10,28 @@ export class SociosComponent implements OnInit {
 
   beneficios:any[];
   socios:any[];
+  hoveredArea:any;
+  isHover:Boolean;
+
 
   constructor() { }
 
   ngOnInit() {
-    //$('#carr').carousel({fullWidth: true});
     this.beneficios=[{'imagen':'../assets/socios/moduloweb.png', 'texto':'MÓDULO WEB PARA CAPTURA DE ÓRDENES'},
     {'imagen':'../assets/socios/apoyo.png', 'texto':'APOYO EN CAMPAÑAS (PRECIOS ESPECIALES)'},
     {'imagen':'../assets/socios/credito.png', 'texto':'CRÉDITO'},
     {'imagen':'../assets/socios/descuento.png', 'texto':'DESCUENTOS EN ESTUDIOS DE LABORATORIO PARA SU PERSONAL'},
     {'imagen':'../assets/socios/integracion.png', 'texto':'INTEGRACIÓN DE SISTEMA DE LABORATORIO'},
     {'imagen':'../assets/socios/reporte.png', 'texto':'REPORTE DE RESULTADOS PERSONALIZADO'}];
+  }
+ 
+  //Función para colorear on hover
+  activo(item:any,flag:Boolean):void{
+    this.hoveredArea=item; 
+    this.isHover=flag;
+    if(!this.isHover){
+      this.hoveredArea="";
+    }
   }
 
   anterior():void{
