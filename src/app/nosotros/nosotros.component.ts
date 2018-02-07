692391b79d1f3d4553342767b0e62b366e50d393
Cristian Espiritu <cxistian@gmail.com>
2018-02-07 12:10:33 -0600

    autoscroll

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -8,32 +8,39 @@ declare var $:any;
 })
 export class NosotrosComponent implements OnInit {
 
+  invisible:boolean=false;
   constructor( ) {
 
    }
 
    @HostListener('window:scroll', ['$event']) onScrollEvent($event){
-    var speed = 150;
+    var speed = 400;
     var s = $(document).scrollTop();
-    console.log(s);
+    
+    console.log(this.invisible);
 
+    if(s>130 && s<189 && !(this.invisible)){
+      this.invisible=true;
+      $('#titulo').hide();
+      $('#parImg').hide();
+      $('#logos').hide();
+    }
 
-    if(s > 500){
+    if(s > 189){
       console.log("MAYOR");
       $('#titulo').fadeIn(speed);
     }
 
-    if(s > 600){
+    if(s > 500){
       $('#parImg').fadeIn(speed);
     }
 
-    if(s > 800){
+    if(s > 890){
       $('#logos').fadeIn(speed);
     }    
   } 
 
   ngOnInit() {
-
 
   }
 
