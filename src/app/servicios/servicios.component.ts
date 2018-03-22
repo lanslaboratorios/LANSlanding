f4322b729dec6f89a9569c45aa291b275ef4c6ec
CEspiritu <cespiritu@lans.mx>
2018-03-22 16:33:50 -0600

    [MOD] nuevo texto areas especialidad

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -22,121 +22,152 @@ area: any;
     this.areas = [ 'Biología Molecular', 'Química Clínica', 'Microbiología', 'Patología', 'Toxicología',
                 'Neonatología', 'Hematología', 'Inmunología', 'Inmunoquímica'];
     this.Card = {
-      'Química Clínica': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
-      'img': '../../assets/servicios/lans_biologia_molecular.png',
-      'parrafoPrincipal': 'Química Clínica es una de las áreas principales del laboratorio.' +
-      'Se procesan estudios de rutina y especiales los cuales son de gran ayuda para el médico,' +
-      ' ya que con esto puede proporcionarle al paciente un buen diagnóstico, seguimiento y control de tratamiento,' +
-      ' así como también le es de utilidad para la prevención y la investigación de enfermedades.' +
-      ' En el laboratorio clínico se realizan diversos estudios, entre los cuales tenemos: perfil de lípidos,' +
-      ' perfil cardiaco, perfil renal, enzimas cardiacas, drogas de abuso, inmunoproteínas, etc.',
-      'parrafo2': 'Para que un resultado sea confiable las muestras deben ser de calidad y ' +
-      'debe de cumplirse con un proceso de control de calidad interno, esto es, se debe de cumplir' +
-      ' con las fases pre-analítica, analítica y post-analítica. ',
-      'parrafo3': 'El área de Química Clínica cuenta con personal altamente capacitado y con ' +
-      'la experiencia suficiente para realizar las actividades correspondientes. En el área se ' +
-      'cuenta también con dos instrumentos de medición directa de alta tecnología. Se utilizan ' +
-      'reactivos que previamente han pasado por un control de calidad interno.',
-    },
       'Biología Molecular': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
+      'img': '../../assets/servicios/lans_biologia_molecular.png',
+      'parrafoPrincipal': 'El Diagnóstico Molecular ha revolucionado el diagnóstico clínico ' +
+      'y es una de las áreas del laboratorio clínico con mayor crecimiento y desarrollo.',
+      'parrafo2': 'Las nuevas tecnologías, rápidas y precisas, han hecho de esta área una ' +
+      'herramienta clave para muchas áreas de la salud, en particular ha sido de gran utilidad en ' +
+      'la detección de enfermedades infecciosas y genéticas.',
+      'parrafo3': 'Algunas de las pruebas que procesamos en esta área mediante PCR en tiempo real son:',
+      'pruebas' : ['Carga Viral de Citomegalovirus',
+        'Carga Viral de VIH',
+        'Carga Viral de Hepatitis C',
+        'Detección de VPH de alto riesgo',
+        'Detección Mycobacterium Tuberculosis',
+        'Detección de Chlamydia Trachomatis'
+        ]
+    },
+      'Química Clínica': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal':  'En la actualidad, el diagnóstico molecular se ha ' +
-      'enfocado principalmente en el diagnóstico de enfermedades infecciosas (50-60%), sin embargo, ' +
-      'existe un aumento progresivo de técnicas moleculares en el área de cáncer y enfermedades ' +
-      'genéticas, convirtiendo al diagnóstico molecular en una de las áreas de mayor ' +
-      'dinamismo y crecimiento, revolucionando las estrategias para el tratamiento de diversas ' +
-      'patologías y condiciones de salud, ofreciendo técnicas con altos estándares de calidad que ' +
-      'entregan al equipo clínico información crítica para el cuidado de los pacientes.',
-      'parrafo2': 'Las enfermedades infecciosas se han transformado en la “punta de lanza” para el ' +
-      'desarrollo de test de diagnóstico molecular, siendo más del 50 % de las técnicas disponibles hoy en día.',
-      'parrafo3': 'El área de biología molecular cuenta con un amplificador de la más alta tecnología ' +
-      'y que apoya en el seguimiento y diagnóstico de enfermedades infeccionas como carga viral de ' +
-      'CMV, HIV, HCV, Mycobacterium tuberculosis, Chlamydia trachomatis, Neisseria gonorrheae y detección de VPH de alto riesgo.',
+      'parrafoPrincipal':  'El área de Química Clínica integra las bases fundamentales de ' +
+      'la química y la bioquímica relacionada con los mecanismos de la fisiología y la patología ' +
+      'para evaluar los cambios que ocurren en el organismo.',
+      'parrafo2': 'Contamos con plataformas de medición directa de alta tecnología, que utilizan ' +
+      'metodologías como fotometría, potenciometría y turbidimetría.',
+      'parrafo3': 'Algunas de las pruebas que procesamos en esta área son:',
+      'pruebas' : ['Electrolitos Séricos (Na, Cl, K, Mg, P, Ca)',
+        'CK Total y CK-MB',
+        'Bióxido de Carbono (CO2)',
+        'Colinesterasa',
+        'Apolipoproteínas A1 y B',
+        'Cistatina C'
+        ]
     },
       'Microbiología': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal':  '',
+      'parrafoPrincipal':  'Nuestro departamento de microbiología utiliza tecnología ' +
+      'de colorimetría avanzada para identificación microbiana y cuenta con una amplia experiencia.',
       'parrafo2': '',
-      'parrafo3': '',
+      'parrafo3': 'Algunas de las pruebas que procesamos en esta área son:',
+      'pruebas' : ['Mycoplasma Hominis',
+        'Ureaplasma Urealyticum',
+        'Hemocultivos',
+        'Quantiferón',
+        'Prueba de VIH 4ta generación'
+        ]
     },
       'Patología': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal': '',
+      'parrafoPrincipal': 'El área de patología se encarga del correcto diagnóstico ' +
+      'histológico y citológico por medio de morfología, inmunohistoquímica, inmunomarcadores de ' +
+      'proliferación y mutaciones, entre otros  para definir las causas, desarrollo y pronóstico de las enfermedades. ',
       'parrafo2': '',
-      'parrafo3': '',
+      'parrafo3': 'Algunas de las pruebas que procesamos en esta área son:',
+      'pruebas' : ['Citología Cérvico-Vaginal (Papanicolaou)',
+        'Papanicolaou Cérvico-Vaginal en Base Líquida',
+        'Papanicolaou de Líquidos Biológicos Diversos',
+        'Detección de HER2, KRAS',
+        'Citología por Aspiración con Aguja Fina'
+        ]
     },
       'Toxicología': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal': '',
+      'parrafoPrincipal': 'El área de Toxicología se encarga de la detección y monitoreo del ' +
+      'consumo de drogas (terapéuticas o de abuso) y exposición a tóxicos industriales o ambientales. ' +
+      'La orina es el espécimen más usado, el más efectivo para la determinación y para el que se dispone ' +
+      'de numerosas técnicas.',
       'parrafo2': '',
-      'parrafo3': '',
+      'parrafo3': 'Algunas de las pruebas que procesamos en esta área son:',
+      'pruebas' : ['Anfetaminas', 'Benzodiacepinas', 'Canabinoides (THC)', 'Cocaína', 'Morfina (Opiáceos)',
+        'Barbituratos', 'Ciclosporina', 'Tacrolimus', 'Sirolimus', 'Everolimus',
+        'Plomo', 'Aluminio', 'Zinc', 'Cadmio', 'Manganes'
+      ]
     },
       'Neonatología': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal': 'El Tamiz Neonatal consiste en una serie de pruebas de laboratorio que se ' +
-      'realizan a partir de una muestra sanguínea tomada del talón del pie del recién nacido o del ' +
-      'cordón umbilical, con el objeto de descubrir y tratar oportunamente enfermedades genéticas ' +
-      'graves e irreversibles que no se detectan en el nacimiento, ni siquiera con una revisión ' +
-      'médica muy cuidadosa.',
-      'parrafo2': 'Algunos ejemplos de las enfermedades que pueden ser detectadas son: hipotiroidismo ' +
-      'congénito, fibrosis quística, hiperplasia adrenal congénita, galactosemia, aminoacidopatías ' +
-      '(fenilcetonuria (PKU), tirosinemia hereditaria, enfermedad de jarabe de maple, etc.), ' +
-      'acidemias orgánicas, transtornos en la oxidación de ácidos grasos, hemoglobinopatías (hemoglobinas anormales) y muchas más.',
-      'parrafo3': 'En LANS laboratorios nos preocupamos por brindar la mayor atención y la mejor ' +
-      'calidad a todos nuestros pacientes y es por ello que contamos con personal altamente ' +
-      'preparado, y además de estar certificados por el Programa de Aseguramiento de la Calidad en ' +
-      'Tamiz Neonatal del CDC (Centro para el Control y Prevención de Enfermedades).',
+      'parrafoPrincipal': 'El área de Neonatología se encarga del diagnóstico de las enfermedades ' +
+      'del ser humano dentro de los primeros 28 días de vida.',
+      'parrafo2': 'En LANS Laboratorios apoyamos este diagnóstico a través del Tamiz Metabólico Neonatal, ' +
+      'que consiste en una serie de pruebas que se realizan a partir de una muestra sanguínea tomada del ' +
+      'talón del pie del recién nacido, con el objeto de detectar oportunamente enfermedades metabólicas.',
+      'parrafo3': 'Algunos ejemplos de las enfermedades que pueden ser detectadas son:',
+      'pruebas' : ['hipotiroidismo congénito', 'fibrosis quística', 'hiperplasia adrenal congénita', 
+      'galactosemia', 'aminoacidopatías (fenilcetonuria (PKU), tirosinemia hereditaria, enfermedad de jarabe de maple, etc.)',
+      'acidemias orgánicas', 'transtornos en la oxidación de ácidos grasos',
+      'hemoglobinopatías (hemoglobinas anormales)'
+      ]
     },
       'Hematología': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal': 'En esta Área la citometría hemática es una prueba multiparamétrica ' +
-      'rutinaria altamente solicitada ya que aporta gran información en la ' +
-      'evaluación clínica del paciente, el área cuenta con tecnología ' +
-      'avanzada para el proceso de esta prueba, se ha incorporado un auto-analizador de última ' +
-      'generación que permite visualizar de manera más práctica todos los parámetros' +
-      ', así como la distribución leucocitaria realizada por tinción ' +
-      'inmunohistoquímica de peroxidasa y los gráficos de distribución eritrocitaria permitiendo ' +
-      'clasificar las anemias de manera más práctica.',
-      'parrafo2': 'De igual forma para conocer el estado inmunológico del paciente y para saber la ' +
-      'efectividad en el tratamiento de pacientes con HIV, se cuenta con un equipo de citometría de ' +
-      'flujo de 4 colores en donde se realiza la determinación de la subpoblación linfocitaria.',
-      'parrafo3': 'En esta área también se cuenta con un equipo automatizado de última generación ' +
-      'para el proceso de pruebas de rutina como Tiempo de protrombina, Tiempo parcial de ' +
-      'tromboplastina, Fibrinógeno y pruebas especializadas como Factores de la coagulación, ' +
-      'Anticoagulante lúpico, Proteína S y C de la coagulación.',
+      'parrafoPrincipal': 'Dentro del área de Hematología se estudian los elementos formes de la ' +
+      'sangre y sus precursores, así como de los trastornos estructurales y bioquímicos, que pueden ' +
+      'conducir a una enfermedad.',
+      'parrafo2': 'En LANS contamos con analizadores de última generación que permiten visualizar de ' +
+      'manera eficaz estos elementos con gráficos de distribución eritrocitaria permitiendo clasificar ' +
+      'las anemias y tinciones de peroxidasa para la cuenta diferencial que brindan información clínica adicional.',
+      'parrafo3': 'En esta área también se cuenta con equipo automatizado de última generación para el proceso ' +
+      'de pruebas de Coagulación básica y especializada como: ',
+      'pruebas' : ['Factores de la coagulación',
+        'Anticoagulante lúpico',
+        'Proteína S de la coagulación',
+        'Proteína S de la coagulación',
+        'Factor V de Leiden'
+      ]
     },
       'Inmunología': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal': 'El área de Inmunología/ ELISAS, es una de las áreas complementarias ' +
-      'en el diagnóstico clínico, implementado técnicas de alta sensibilidad y especificidad en la ' +
-      'detección de enfermedades infecciosas, alérgicas y autoinmunes principalmente.ELISA es una de ' +
-      'las técnicas más utilizadas para identificar o ' +
-      'confirmar la especificidad de los anticuerpos presentes en las muestras de los pacientes con múltiples enfermedades.',
-      'parrafo2': 'Además, por su fácil estandarización, manejo y variedad de antígenos disponibles. ' +
-      'Si bien existen diferentes tipos de ELISA, el más utilizado en el laboratorio de diagnóstico ' +
-      'de autoinmunidad es el ELISA indirecto, el cual se fundamenta en el reconocimiento de los ' +
-      'anticuerpos específicos presentes en las muestras de los pacientes, mediante un anticuerpo ' +
-      'dirigido contra la región Fc humana de cualquier isotipo (IgG, IgA o IgM).',
-      'parrafo3': 'La técnica puede ser cualitativa si solo se requiere conocer si existen o no ' +
-      'anticuerpos con reactividad por determinado antígeno o cuantitativa si se requiere conocer ' +
-      'la cantidad de anticuerpos presentes en las muestras, para lo cual el ensayo debe incluir ' +
-      'una curva patrón de reactividad especifica.',
+      'parrafoPrincipal': 'Es la ciencia que estudia todos los aspectos del sistema inmunitario normal y ' +
+      'patológico, e incluye las enfermedades en las que los mecanismos inmunitarios no actúan adecuadamente, ' +
+      'bien sea por razones genéticas o adquiridas.',
+      'parrafo2': 'En el caso de las enfermedades autoinmunes, las técnicas inmunológicas son de gran ayuda ya ' +
+      'que permiten la detección de auto-anticuerpos; para esto existen diferentes técnicas, sin embargo, ' +
+      'una de las más utilizados es ELISA, que se fundamenta en el reconocimiento de los anticuerpos específicos ' +
+      'presentes en las muestras de los pacientes, mediante un anticuerpo dirigido contra la región ' +
+      'Fc humana (IgG, IgA o IgM).',
+      'parrafo3': 'Algunas de las pruebas que procesamos en esta área son:',
+      'pruebas' : ['Ac. Anti-Citoplasma de Neutrófilos Citoplasmáticos (c-ANCA)',
+        'Ac. Anti-ADN de Doble Cadena (Nativo)',
+        'Ac. Anti-Citoplasma de Neutrófilos Perinucleares (p-ANCA)',
+        'Ac. Anti-Fosfolípidos IgG',
+        'Ac. Anti-Fosfolípidos IgM',
+        'Ac. Anti-Smith',
+        'Hormona Anti-Mulleriana',
+        'Ac. Anti-Treponema pallidum (FTA-ABS)',
+        'Ac. Anti-Nucleares (ANA)',
+        'Ac. Anti-Músculo Liso',
+        'Anticuerpos Anti-DNA Nativo Cuantitativo',
+        'Ac. Anti-Centrómero',
+        'Ac. Anti-Mitocondriales'
+      ]
     },
       'Inmunoquímica': {'bg': '../../assets/servicios/lans_biologia_molecular.png',
       'img': '',
-      'parrafoPrincipal': 'El área de Inmunoquímica se encuentra conformada por cuatro equipos ' +
-      'de vanguardia. Aquí se procesan la mayor parte de las pruebas realizadas en el laboratorio. ' +
-      'Entre las pruebas que se realizan se encuentran: Serología infecciosa, Marcadores cardíacos, ' +
-      'Marcadores Tumorales, Pruebas de Fertilidad, Perfil Torch, Perfil Tiroideo, Función adrenal, ' +
-      'Metabolismo óseo, Biomarcador de respuesta inflamatoria, Drogas terapéuticas, etc.',
-      'parrafo2': 'Éstas pruebas ayudan al diagnóstico de diversas enfermedades, son también de ' +
-      'mucha utilidad para que el médico pueda llevar a cabo un correcto seguimiento al tratamiento del paciente.',
-      'parrafo3': 'Para poder obtener un resultado seguro se debe cumplir correctamente con algunos ' +
-      'criterios; El paciente debe atender a las indicaciones que se proporcionan antes de ' +
-      'la toma de muestra, esto es para que la muestra que se obtenga se de calidad. El personal de ' +
-      'laboratorio debe de identificar, almacenar y transportar dicha muestra correctamente, así ' +
-      'como también debe de cerciorarse que la cantidad sea suficiente. Se debe cumplir también de ' +
-      'manera adecuada la fase analítica y para esto se lleva a cabo el proceso de control de calidad interno.',
+      'parrafoPrincipal': 'La Inmunoquímica la podemos definir como el estudio de la constitución ' +
+      'química de los antígenos y de los anticuerpos, o como la parte de la química que estudia las ' +
+      'reacciones de inmunidad antígeno-anticuerpo.',
+      'parrafo2': 'En realidad, se trata de una subdivisión de la inmunología, que se ha distinguido ' +
+      'por ser automatizada y utilizar metodologías que ofrecen una mayor sensibilidad y especificidad.',
+      'parrafo3': 'Contamos con plataformas analíticas de última generación que utilizan la metodología ' +
+      'de quimioluminiscencia para la determinación de estudios como: ',
+      'pruebas' : ['Antígeno Cyfra 21',
+      'Ca 15-3',
+      'Ca 125',
+      'TORCH',
+      'Panel de Hepatitis',
+      'Vitamina D',
+      'Tiroideos'
+      ]
     }
     };
     this.areas.sort();
